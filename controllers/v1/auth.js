b3b4c538f2df72600b3413e368f5293483633876
arminEX <stob5r11@gmail.com>
2025-02-11 18:26:59 +0330

    #7 Auth

---
+++ controllers/v1/auth.js
@@ -0,0 +1,8 @@
+const userModel = require('../../models/user');
+
+exports.register = async (req, res) => { };
+exports.login = async (req, res) => { };
+exports.getMe = async (req, res) => { };
+
+
+
