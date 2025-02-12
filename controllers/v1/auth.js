33398eb281e01a272ee86c9be44434cf3f677e82
arminEX <stob5r11@gmail.com>
2025-02-12 13:01:10 +0330

    #11 Hashing the user password

--- controllers/v1/auth.js
+++ controllers/v1/auth.js
@@ -1,5 +1,6 @@
 const userModel = require('../../models/user');
 const registerValidator = require('../../validators/register');
+const bcrypt = require('bcrypt');
 
 exports.register = async (req, res) => {
     const validationResult = registerValidator(req.body);
@@ -14,11 +15,23 @@ exports.register = async (req, res) => {
     const isUserExist = await userModel.findOne({
         $or: [{ username }, { email }],
     });
+
     if (isUserExist) {
         return res.status(409).json({
             message: "username or email already exist",
         });
     };
+
+    const countOfUsers = await userModel.count();
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const user = new userModel({
+        email,
+        username,
+        name,
+        phone,
+        password: hashedPassword,
+        role: countOfUsers > 0 ? "USER" : "ADMIN",
+    });
 };
 exports.login = async (req, res) => { };
 exports.getMe = async (req, res) => { };
