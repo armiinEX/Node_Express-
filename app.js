cfaa1a3d7826393604ace231dd85526c240f1520
arminEX <stob5r11@gmail.com>
2025-02-12 08:18:27 +0330

    #9 Default middleware

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require('express');
+const authRouter = require('./routes/v1/auth');
+const cors = require('cors');
+const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use("/courses/covers", express.static(path.join(__dirname, "public", "courses", 'covers')));
+app.use(cors());
+app.use('/v1/auth', authRouter);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 module.exports = app;
--- validators/register.js
+++ validators/register.js
@@ -5,6 +5,7 @@ const schema = {
     name: { type: "string", min: 3, max: 255 },
     username: { type: "string", min: 3, max: 100 },
     email: { type: "email", min: 8, max: 100 },
+    phone: { type: "number", max: 11 },
     password: { type: "string", min: 8, max: 100 },
     confirm_password: { type: "equal", field: "password" },
     $$strict: true
