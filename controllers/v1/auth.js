29ffcaec669e15f1420f9c9222520d489d76cf2b
arminEX <stob5r11@gmail.com>
2025-02-12 10:25:10 +0330

    #10 Registration implementation

--- controllers/v1/auth.js
+++ controllers/v1/auth.js
@@ -1,6 +1,25 @@
 const userModel = require('../../models/user');
+const registerValidator = require('../../validators/register');
 
-exports.register = async (req, res) => { };
+exports.register = async (req, res) => {
+    const validationResult = registerValidator(req.body);
+    if (validationResult != true) {
+        return res.status(422).json({
+            message: "Validation failed",
+            errors: validationResult
+        });
+    };
+
+    const { name, username, email, phone, password } = req.body;
+    const isUserExist = await userModel.findOne({
+        $or: [{ username }, { email }],
+    });
+    if (isUserExist) {
+        return res.status(409).json({
+            message: "username or email already exist",
+        });
+    };
+};
 exports.login = async (req, res) => { };
 exports.getMe = async (req, res) => { };
 
