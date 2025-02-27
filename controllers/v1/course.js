875729499c8d29c464d1f87d2a141ba1d58d5c23
arminEX <stob5r11@gmail.com>
2025-02-27 10:55:33 +0330

    #36 Get all session

--- controllers/v1/course.js
+++ controllers/v1/course.js
@@ -49,3 +49,9 @@ exports.createSession = async (req, res) => {
 
     return res.status(201).json(session);
 };
+
+exports.getAllSessions = async (req, res) => {
+    const sessions = await sessionModel.find({}).populate("course", "name").lean();
+
+    return res.json(sessions);
+};
--- routes/v1/course.js
+++ routes/v1/course.js
@@ -19,7 +19,11 @@ router.route("/:id/sessions")
         // multer({ storage: multerStorage, limits: { fileSize: 1000000000 } }).single('video'),
         authMiddleware,
         isAdminMiddleware,
-        cousrsesController.createSession)
+        cousrsesController.createSession
+    );
+
+
+router.route("/sessions").get(authMiddleware, isAdminMiddleware, cousrsesController.getAllSessions)
 
 
 module.exports = router;
