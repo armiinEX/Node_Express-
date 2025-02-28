4d64337925b0d74a782de778589ddbc08bbb94f3
arminEX <stob5r11@gmail.com>
2025-02-28 23:03:10 +0330

    #39 Create a comment

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const usersRouter = require('./routes/v1/user');
 const categoryRouter = require('./routes/v1/category');
 const coursesRouter = require('./routes/v1/course');
+const commentsRouter = require("./routes/v1/comment")
 
 
 const app = express();
@@ -21,6 +22,8 @@ app.use('/v1/auth', authRouter);
 app.use('/v1/users', usersRouter);
 app.use('/v1/category', categoryRouter);
 app.use('/v1/courses', coursesRouter); 
+app.use('/v1/comments', commentsRouter); 
+
 
 
 module.exports = app;
---
+++ routes/v1/comment.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const authMiddleware = require("./../../middlewares/auth");
+const commentController = require("./../../controllers/v1/comment");
+const isAdminMiddleware = require("./../../middlewares/isAdmin");
+
+router.route("/").post(authMiddleware, commentController.create);
+
+module.exports = router;  // ✅ تایپ درست
